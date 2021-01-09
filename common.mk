d8aba9bc0af134352352f6e107c7b3708910e19d
Chandra Chaganti <chandra.chaganti@hotmail.com>
2021-01-09 06:03:49 +0100

    sm8250-common: Switch back to bool for FOD

--- common.mk
+++ common.mk
@@ -279,7 +279,9 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.oneplus \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus \
+    vendor.oneplus.fingerprint.extension@1.0 \
+    vendor.oneplus.hardware.display@1.0
 
 # GPS
 PRODUCT_COPY_FILES += \
