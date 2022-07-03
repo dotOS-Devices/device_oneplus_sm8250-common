e81287f70163d4203331c787b63ede315c662761
AnierinB <anierin@evolution-x.org>
2022-07-03 13:53:00 +0900

    sm8250-common: Configure & ship OPlusExtras
    - Device specific features are set in their respective trees.
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- common.mk
+++ common.mk
@@ -222,6 +222,7 @@ PRODUCT_PACKAGES += \
     init.oplus.hw.rc \
     init.oplus.hw.rc.recovery \
     init.oplus.rc \
+    init.oplus_extras.rc \
     init.oplus.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
@@ -295,6 +296,11 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# OPlusExtras
+PRODUCT_PACKAGES += \
+    OPlusExtras \
+    tri-state-key-calibrate
+
 # Overlays
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
@@ -306,6 +312,7 @@ PRODUCT_PACKAGES += \
     DotSettingsResCommon \
     DotSystemUIResCommon \
     FrameworksResTarget \
+    OPlusExtrasResCommon \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
     WifiResTarget
